a0a3d3390283550084613387b4574bef03df1f63
hyy <1021509854@qq.com>
2021-11-15 22:02:16 +0800

    up

--- src/main/java/com/hg/hyy/entity/StudentRepository.java
+++ src/main/java/com/hg/hyy/entity/StudentRepository.java
@@ -2,11 +2,11 @@ package com.hg.hyy.entity;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "student", path = "student")
+// @RepositoryRestResource(collectionResourceRel = "student", path = "student")
 public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
 
     List<Student> findByStudentName(@Param("studentName") String studentName);
